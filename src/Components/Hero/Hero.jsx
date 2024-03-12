8f4159a9512806c32b48de953c17c29f24e23822
Viacheslav Liabin <R9514633542@gmail.com>
2024-03-12 21:15:02 +0500

    'herobegin'

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          accusamus fugiat excepturi quae, placeat cupiditate dolore ad
+          perferendis, velit iusto ut culpa commodi hic inventore dolore?
+          <button className="btn">Explore more</button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
