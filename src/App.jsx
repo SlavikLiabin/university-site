6479c1ffc69819fcb138759f3d878683c1566ff7
SlavikLiabin <r9514633542@gmail.com>
2024-03-17 12:28:57 +0500

    'campus'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import About from "./Components/About/About"
+import Campus from "./Components/Campus/Campus"
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 import Programs from "./Components/Programs/Programs"
+import Testimonials from "./Components/Testimonials/Testimonials"
 import Title from "./Components/Title/Title"
 
 const App = () => {
@@ -13,6 +15,10 @@ const App = () => {
         <Title subTitle='Our PROGRAM' title='What we offer'/>
         <Programs/>
         <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='Testimonials' title='What Student Says'/>
+        <Testimonials/>
       </div>
     </div>
   )
